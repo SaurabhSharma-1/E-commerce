2af952b5d1eca2a48588dea34de9551dc2aaba79
SaurabhSharma-1 <123999022+SaurabhSharma-1@users.noreply.github.com>
2023-06-18 13:28:30 +0530

    Add files via upload

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p>
+                    You love your headphones, we geddit! But finding the perfect headphones today can be as difficult as finding a needle in a haystack, 
+                    especially if you are not familiar with all the technology that comes with it. 
+                    we know you still buy it solely on the basis of how slick it looks and how powerful the sound is.
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta" onClick={()=> window.scrollTo(0,5000)}>Read More</div>
+                        <div className="banner-cta v2" onClick={()=> window.scrollTo(0,500)}>Shop Now</div> 
+                    </div>
+                </div>
+                <img className="banner-img" src = {BannerImg} alt = "" />
+            </div> 
+           </div>
+};
+
+export default Banner;
